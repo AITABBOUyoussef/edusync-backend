0e07f0c8a47f1266f2d11c716d8ba7372dd73526
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-04-20 12:57:52 +0100

    ajouter header and footer includes to index page; create login page

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php 
+include '../includes/header.php'; 
+?>
+
+<div class="text-center mt-20">
+    <h1 class="text-4xl font-bold mb-4">Bienvenue sur EduSync</h1>
+    <p class="text-lg text-gray-600">Le système de gestion scolaire sécurisé.</p>
+</div>
+
+<?php 
+include '../includes/footer.php'; 
+?>
