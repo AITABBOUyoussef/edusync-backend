54f7a9697b3938885db4caab82b4a664b62b76a9
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-04-22 12:03:19 +0100

    db.php to remove commented notes for clarity and maintainability

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,10 @@
 <?php
 $host = 'localhost';
-$dbname = 'edusync'; // تأكد بلي السمية هنا هي نفسها لي صاوبتي فـ phpMyAdmin
+$dbname = 'edusync'; 
 $username = 'root';
 $password = '';
 
 try {
-    // ⚠️ المشكل كيكون غالبا فهاد السطر 
-    // تأكد بلي كاتب dbname=$dbname وماكاينش شي فراغ بيناتهم
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
