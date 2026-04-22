477729f519713106059f2c8bd864b3c1602aa4e0
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-04-22 11:34:50 +0100

    db.php for improved readability and consistency in PDO connection handling

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,17 @@
-<?php 
-$host = 'localhost'; 
-$dbname = 'edusync';
+<?php
+$host = 'localhost';
+$dbname = 'edusync'; // تأكد بلي السمية هنا هي نفسها لي صاوبتي فـ phpMyAdmin
 $username = 'root';
 $password = '';
 
 try {
-    $pdo = new PDO("mysql:$host;dbname=$dbname;charset=utf8", $username, $password );
+    // ⚠️ المشكل كيكون غالبا فهاد السطر 
+    // تأكد بلي كاتب dbname=$dbname وماكاينش شي فراغ بيناتهم
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-}catch(PDOException $e){
-    die("Erreur de connexion : ".$e->getMessage());
+    
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
 }
-
 ?>
