7158c6a11b9ad4171ba9fcb63fa07a6076076b5e
Abdellah <hibazekraoui23@email.com>
2026-04-28 11:40:36 +0100

    creating the adduser func

--- admin/functions.php
+++ admin/functions.php
@@ -1,2 +1,11 @@
 <?php 
 require "./connectDB.php" ;
+function adduser($conn,$fullName,$Email,$password, $role){
+    try {
+    $sql = "INSERT INTO users (nom,Email,password,role) VALUES (?,?,?,?)  ";
+    $stm=$conn->prepare($sql);
+    $stm->execute([$fullName,$Email,$password, $role]);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
